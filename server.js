9efcdf2b5488da6b24825bf5f24757c44bce461c
Susana Muñoz <su.munozh@gmail.com>
2023-07-25 20:21:56 -0400

    guardar archivo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
 const app = express();
+const upload = multer({ dest: "public/img/" });
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -10,9 +12,24 @@ app.get("/",(req,res)=>{
     res.sendFile("index");
 });
 
-app.post("/", (req, res)=>{
+
+
+app.post("/", upload.single("archivo"), (req, res)=>{
     console.log(req.body);
+    console.log(req.file);
     res.send("Recibido <br><a href='/'>Volver</a>");
 });
 
-app.listen(3000, ()=>console.log("Puerto levantado"))
+app.listen(3000, ()=>console.log("Puerto levantado"));
+
+/*   JAVIER
+
+const upload = multer({ dest: "public/img/" });
+
+app.post("/", upload.single("archivo"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file); // This will log the uploaded file information
+    res.send("Recibido <br><a href='/'>Volver</a>");
+  });
+
+  */
